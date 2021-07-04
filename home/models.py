961a23d3d79f1cafe5e99d1386d10ba9317a28cf
Peter Paul Kiefer <dafisppk@gmail.com>
2021-07-04 09:09:37 +0200

    some cleanups and new domains added
    * blog
    * explorer
    * member
    * projects
    * skils

--- home/models.py
+++ home/models.py
@@ -3,29 +3,10 @@ from wagtail.core.models import Page
 
 class HomePage(Page):
     """
-    The start page of the Skill-DB site. 
-    This page must only exist one time for the site.
+    The start page of the Skill-DB site. Visitors who are not logged in
+    are redirected to this page. This page must NOT exist more than one time for the site.
     """
 
     max_count = 1
     
-
-class OverviewPage(Page):
-    """
-    Overview pages are used to list profile pages.
-    """
-
-class ProfilePage(Page):
-    """
-    Profile Pages are used to manage developer, consultant ond prospective customer 
-    profiles.
-    """
-
-class ExplorerPage(Page):
-    """
-    Find profiles in terms of skills, knowledge 
-    and projects by required" or optional feastures.
-    """
-
-
 
--- skilldb/settings/base.py
+++ skilldb/settings/base.py
@@ -9,6 +9,11 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
+    'blog',
+    'explorer',
+    'member',
+    'projects',
+    'skills',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
